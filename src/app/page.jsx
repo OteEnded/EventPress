ec5e45c1dac0095030f6159f841a1f3cbea40622
OteEnded <ratnaritjumnong@gmail.com>
2025-03-01 15:41:25 +0700

    fix: image not showup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import ThemeToggle from "../ui/components/ThemeToggle";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 // import "./globals.css";
 
 export const metadata = {
@@ -11,14 +11,15 @@ export const metadata = {
 export default async function HomePage() {
     return (
         <>
-        {/* navbar */}
+            {/* navbar */}
             <div className="p-4 flex justify-between items-center">
                 <div className="p-4 gap-4 flex">
                     <Image
-                        src={"/globe.svg"}
-                        width={22}
-                        height={22}
-                        alt="logo"
+                        src="/eventpress-logo.png"
+                        alt="Event Press logo"
+                        width={180}
+                        height={38}
+                        priority
                     />
                     <ThemeToggle />
                 </div>
@@ -41,9 +42,12 @@ export default async function HomePage() {
             <div className="min-h-screen flex flex-col items-center justify-center bg-[#5E9BD6] text-gray-700 dark:bg-gray-900 dark:text-gray-100 px-6">
                 {/* Header Section */}
                 <header className="text-center mb-12">
-                    <h1 className="text-5xl text-white font-extrabold mb-4">EventPress</h1>
+                    <h1 className="text-5xl text-white font-extrabold mb-4">
+                        EventPress
+                    </h1>
                     <p className="text-lg text-gray-100 dark:text-gray-300 max-w-2xl">
-                    แพลตฟอร์มช่วยประกาศอีเวนท์ รู้ลึกถึงกิจกรรมในแต่ละบูธ และสิ่งที่คุณจะได้พบ
+                        แพลตฟอร์มช่วยประกาศอีเวนท์ รู้ลึกถึงกิจกรรมในแต่ละบูธ
+                        และสิ่งที่คุณจะได้พบ
                     </p>
                 </header>
 
@@ -53,7 +57,9 @@ export default async function HomePage() {
                         ทำไมต้อง EventPress?
                     </h2>
                     <p className="text-gray-700 dark:text-gray-300">
-                        EventPress ช่วยให้ผู้จัดงานสามารถสร้างหน้าเว็บไซต์งานที่กำหนดเองได้อย่างง่ายดาย โดยไม่จำเป็นต้องเขียนโค้ด—เพียงแค่ใช้ส่วนติดต่อที่ทรงพลังและใช้งานง่าย
+                        EventPress
+                        ช่วยให้ผู้จัดงานสามารถสร้างหน้าเว็บไซต์งานที่กำหนดเองได้อย่างง่ายดาย
+                        โดยไม่จำเป็นต้องเขียนโค้ด—เพียงแค่ใช้ส่วนติดต่อที่ทรงพลังและใช้งานง่าย
                     </p>
 
                     <div className="mt-6 grid md:grid-cols-3 gap-6">
@@ -62,8 +68,9 @@ export default async function HomePage() {
                             <h3 className="font-semibold text-xl">
                                 📌 ตัวสร้างแบบลากและวาง
                             </h3>
-                            <p className="text-sm text-gray-600 dark:text-gray-300 mt-2">                           
-                                สร้างหน้าเว็บไซต์งานที่มีปฏิสัมพันธ์ได้ง่ายๆ ด้วยส่วนประกอบที่สร้างไว้ล่วงหน้า
+                            <p className="text-sm text-gray-600 dark:text-gray-300 mt-2">
+                                สร้างหน้าเว็บไซต์งานที่มีปฏิสัมพันธ์ได้ง่ายๆ
+                                ด้วยส่วนประกอบที่สร้างไว้ล่วงหน้า
                             </p>
                         </div>
 
@@ -83,7 +90,8 @@ export default async function HomePage() {
                                 📊 การวิเคราะห์และการมีส่วนร่วม
                             </h3>
                             <p className="text-sm text-gray-600 dark:text-gray-300 mt-2">
-                                รับข้อมูลเชิงลึกเกี่ยวกับผู้เข้าร่วม การมีส่วนร่วม และผลการดำเนินงานของงาน
+                                รับข้อมูลเชิงลึกเกี่ยวกับผู้เข้าร่วม
+                                การมีส่วนร่วม และผลการดำเนินงานของงาน
                             </p>
                         </div>
                     </div>
@@ -91,14 +99,6 @@ export default async function HomePage() {
 
                 {/* Call to Action */}
                 <section className="text-center">
-                    {/* <h2 className="text-2xl font-bold mb-4">
-                        Start Organizing Your Event Today
-                    </h2>
-                    <p className="text-gray-600 dark:text-gray-300 max-w-xl">
-                        Join other event organizers in streamlining event
-                        management. Create engaging event pages and enhance
-                        attendee experience effortlessly.
-                    </p> */}
                     <Link href="/organizer/register">
                         <button className="px-6 py-3 bg-white text-[#5E9BD6] rounded-lg text-xl font-semibold hover:bg-blue-700 dark:text-white dark:bg-blue-500 dark:hover:bg-blue-400 transition">
                             ลงทะเบียนเลย!
